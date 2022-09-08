4e4c9993849f1b8c4c1bcd2f71c11a7f46db618d
Pablo Lara Cedillo <98928996+PabloCed@users.noreply.github.com>
2022-09-07 19:04:25 -0500

    Add files via upload
    interfaz de raíces

--- MetodoBiseccion/src/metodobiseccion/MetodoBiseccion.java
+++ MetodoBiseccion/src/metodobiseccion/MetodoBiseccion.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package metodobiseccion;
 
-/**
- *
- * @author david
- */
 public class MetodoBiseccion {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Metodo metodo = new Metodo();
+
+        metodo.setFuncion("x^2 - 5");
+        double raiz = metodo.biseccion(2, 2.5, 0.001);
+        System.out.println("La raíz es: " + raiz);
+
+metodo.setFuncion("cos(x)-x");
+        double raiz2 = metodo.biseccion(0, Math.PI/2, 0.001);
+        System.out.println("La raíz es: " + raiz2);
+
+
     }
-    
+
 }
